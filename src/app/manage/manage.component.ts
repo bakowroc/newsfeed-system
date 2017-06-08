6dcd23b7f1f678ba2992b0d1eadf8a8f103a197c
malwina-zeglen <malwina.zeglen@gmail.com>
2017-06-09 00:55:12 +0200

    base of management panel view

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
  
 
 @Component ({
@@ -7,6 +7,32 @@ import {Component} from '@angular/core';
     styleUrls: ['./manage.component.scss']
 })
 
-export default class ManageComponent{
-    searchValue: string;
+export default class ManageComponent implements OnInit{
+   menu: Array<Object>;
+    constructor(){}
+    ngOnInit(){
+        this.menu = [
+           {
+               name: 'Posts', link: '#'
+           },
+            {
+                name: 'Add post', link: '#'
+            },
+            {
+                name: 'Comments', link: '#'
+            },  
+            {
+                name: 'Categories', link: '#'
+            },
+            {
+                name: 'Users', link: '#'
+            },
+            {
+                name: 'Add user', link: '#'
+            },
+            {
+                name: 'Groups', link: '#'
+            }
+        ]
+    }
 } 
